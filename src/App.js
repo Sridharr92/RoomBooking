0bfedd5602fac0ca77882a93f990d108e6b78bb5
R <sridhar.r1@pearson.com>
2020-12-02 12:11:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RoomBooking from './RoomBooking';
+
+function App() {
+  return (
+    <div className="App">
+      <RoomBooking />
+    </div>
+  );
+}
+
+export default App;
